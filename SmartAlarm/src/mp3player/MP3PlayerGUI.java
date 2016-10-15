3bcdf9c9518f85ea138c8c016ba60c6280dce7bc
linh1706 <linh1706@gmail.com>
2016-10-15 01:53:07 -0500

    Create the SmartAlarm project

---
+++ SmartAlarm/src/mp3player/MP3PlayerGUI.java
@@ -0,0 +1,199 @@
+package mp3player;
+
+//All code is added to create the widget. Follow the "Create a Widget" tutorial if you want to know how I did it.
+
+import java.awt.Toolkit;
+
+public class MP3PlayerGUI extends javax.swing.JDialog 
+{
+    Mp3Class MC = new Mp3Class();
+    int xMouse;
+    int yMouse;
+    
+    int width = (Toolkit.getDefaultToolkit().getScreenSize().width / 2) - 185;
+    int height = Toolkit.getDefaultToolkit().getScreenSize().height - 180;
+    
+    
+    
+    public MP3PlayerGUI(java.awt.Frame parent, boolean modal) 
+    {
+        super(parent, modal);
+        initComponents();
+        
+        this.setLocation(width, height);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        Play = new javax.swing.JLabel();
+        SelectFile = new javax.swing.JLabel();
+        Pause = new javax.swing.JLabel();
+        Stop = new javax.swing.JLabel();
+        Loop = new javax.swing.JLabel();
+        Display = new javax.swing.JLabel();
+        Background = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("MP3 Player");
+        setUndecorated(true);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        Play.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        Play.addAncestorListener(new javax.swing.event.AncestorListener() {
+            public void ancestorMoved(javax.swing.event.AncestorEvent evt) {
+            }
+            public void ancestorAdded(javax.swing.event.AncestorEvent evt) {
+                PlayAncestorAdded(evt);
+            }
+            public void ancestorRemoved(javax.swing.event.AncestorEvent evt) {
+            }
+        });
+        Play.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                PlayMouseReleased(evt);
+            }
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                PlayMouseClicked(evt);
+            }
+        });
+        getContentPane().add(Play, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 40, 90, 83));
+
+        SelectFile.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        getContentPane().add(SelectFile, new org.netbeans.lib.awtextra.AbsoluteConstraints(319, 60, 40, 40));
+
+        Pause.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        Pause.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                PauseMouseReleased(evt);
+            }
+        });
+        getContentPane().add(Pause, new org.netbeans.lib.awtextra.AbsoluteConstraints(245, 50, 60, 60));
+
+        Stop.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        Stop.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                StopMouseReleased(evt);
+            }
+        });
+        getContentPane().add(Stop, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 50, 70, 60));
+
+        Loop.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        getContentPane().add(Loop, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, 40, 40));
+
+        Display.setFont(new java.awt.Font("Digital-7", 0, 13)); // NOI18N
+        Display.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        getContentPane().add(Display, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, 330, 20));
+
+        Background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mp3player/Background.png"))); // NOI18N
+        Background.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                BackgroundMouseDragged(evt);
+            }
+        });
+        Background.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                BackgroundMousePressed(evt);
+            }
+        });
+        getContentPane().add(Background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 370, 130));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    
+    private void BackgroundMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BackgroundMouseDragged
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        
+        this.setLocation(x - xMouse, y - yMouse);
+    }//GEN-LAST:event_BackgroundMouseDragged
+
+    private void BackgroundMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BackgroundMousePressed
+        xMouse = evt.getX();
+        yMouse = evt.getY();
+    }//GEN-LAST:event_BackgroundMousePressed
+
+    private void PlayMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PlayMouseReleased
+      
+    }//GEN-LAST:event_PlayMouseReleased
+
+    private void StopMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_StopMouseReleased
+        // TODO add your handling code here:
+        MC.Stop();
+    }//GEN-LAST:event_StopMouseReleased
+
+    private void PauseMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PauseMouseReleased
+        // TODO add your handling code here:
+        MC.Pause();
+    }//GEN-LAST:event_PauseMouseReleased
+
+    private void PlayMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PlayMouseClicked
+        // TODO add your handling code here:
+        MC.Stop2();
+        MC.Play("nothingatall.mp3");
+        Display.setText("When you say no thing at all");
+        
+        
+    }//GEN-LAST:event_PlayMouseClicked
+
+    private void PlayAncestorAdded(javax.swing.event.AncestorEvent evt) {//GEN-FIRST:event_PlayAncestorAdded
+        // TODO add your handling code here:
+    }//GEN-LAST:event_PlayAncestorAdded
+  
+
+    public static void main(String args[]) 
+    {
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MP3PlayerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MP3PlayerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MP3PlayerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MP3PlayerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+   
+        java.awt.EventQueue.invokeLater(new Runnable() 
+        {
+            public void run() 
+            {
+                
+                MP3PlayerGUI dialog = new MP3PlayerGUI(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() 
+                {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) 
+                    {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Background;
+    public static javax.swing.JLabel Display;
+    private javax.swing.JLabel Loop;
+    private javax.swing.JLabel Pause;
+    private javax.swing.JLabel Play;
+    private javax.swing.JLabel SelectFile;
+    private javax.swing.JLabel Stop;
+    // End of variables declaration//GEN-END:variables
+}
